1f12defece06863539f7796926ef010f89331f4a
journeyANDloneliness <rumetta.may@gmail.com>
2023-06-22 05:17:53 +0000

    keluar

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
 
 
 export function keluarDariMenu(text, add){
-	return ["konfirmasi nilai","konfimasi","konfirm","home","sudah","selesai",add].includes(text.toLowerCase())
+	return ["konfirmasi nilai","konfimasi","konfirm","home","sudah","selesai","keluar",add].includes(text.toLowerCase())
 }
