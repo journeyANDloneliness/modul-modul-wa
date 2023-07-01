cadc9ef0ec0182797402a0d921b4b7c74ce325fe
journeyANDloneliness <rumetta.may@gmail.com>
2023-07-01 01:39:41 +0000

    bb

--- common.js
+++ common.js
@@ -1,6 +1,7 @@
 
 
 export function keluarDariMenu(text, add){
+	if(!text) return
 	return ["konfirmasi nilai","konfimasi","konfirm","home","sudah","selesai","keluar",add].includes(text?.toLowerCase())
 }
 
